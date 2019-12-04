3b9d61591aa94139a23f47626cc4341563c6e93b
Mazharenko <mazhor02@gmail.com>
2019-12-04 23:46:03 +0200

    slider

--- js/common.js
+++ js/common.js
@@ -5,4 +5,9 @@ $(function(){
         var newPlaceholder = $(this).data('place');
         $('.search_field input').attr('placeholder', newPlaceholder);
     });
+    $('.say_wrap-review').slick({
+        slidesToShow: 3,
+        slidesToScroll: 2,
+    });
+
 });
