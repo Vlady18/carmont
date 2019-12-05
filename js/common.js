03b7b8aad86e00244c1f4921af6b408c55e90d95
vladhusiev <husiev.vlad@gmail.com>
2019-12-05 17:46:27 +0200

    header

--- js/common.js
+++ js/common.js
@@ -7,7 +7,21 @@ $(function(){
     });
     $('.say_wrap-review').slick({
         slidesToShow: 3,
-        slidesToScroll: 2,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
     });
 
 });
